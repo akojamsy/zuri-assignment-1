214f921029893550dd97bf690670b93405506a52
Akojamsy <akoguitar@gmail.com>
2022-10-27 14:38:17 +0100

    added css and delete

--- week4_and_5/userAuthMySQL/functions.php
+++ week4_and_5/userAuthMySQL/functions.php
@@ -3,7 +3,7 @@
 
 function redirect($pages)
 {
-    header('Location:/zuri/week4_and_5/userAuthMySQL' . $pages);
+    header('Location:/zuri/week4_and_5/userAuthMySQL/' . $pages);
     die;
 }
 
@@ -15,18 +15,7 @@ function authenticate($row)
 
 function is_logged_in()
 {
-    if (!empty($_SESSION['USER'])) return true;
+    if (!empty($_SESSION['username'])) return true;
     return false;
 }
 
-// utility functions
-function old_value($key, $default = '')
-{
-    if (!empty($_POST[$key])) return $_POST[$key];
-    return $default;
-}
-function old_checked_value($key, $default = '')
-{
-    if (!empty($_POST[$key])) return 'checked';
-    return $default;
-}
--- week4_and_5/userAuthMySQL/php/userauth.php
+++ week4_and_5/userAuthMySQL/php/userauth.php
@@ -30,10 +30,9 @@ function registerUser($fullnames, $email, $password, $gender, $country)
 
     //checking for fullnames validity
     if (isset($fullnames)) {
+        $fullnames = stripslashes($_REQUEST['fullnames']);
         if (empty($fullnames)) {
             $errors['fullnames'] = "Fullname is required.";
-        } elseif (!preg_match("/^[a-zA-Z]+$/", $fullnames)) {
-            $errors['fullnames'] = "Fullname can only be letters and no spaces.";
         } elseif (strlen($fullnames) < 8) {
             $errors['fullnames'] = "Fullname cannot be less than 8 characters.";
         }
@@ -59,6 +58,7 @@ function registerUser($fullnames, $email, $password, $gender, $country)
     if (empty($errors)) {
 
         $dob = date('Y-m-d', strtotime('1993-3-12'));
+        $password = password_hash($password, PASSWORD_DEFAULT);
 
         $stmt = $conn->prepare("INSERT INTO students (full_names, email, password, gender, country, dob ) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("ssssss", $fullnames, $email, $password, $gender, $country, $dob);
@@ -69,8 +69,9 @@ function registerUser($fullnames, $email, $password, $gender, $country)
             echo "Error: " . $query . "<br>" . $conn->errors;
         }
     } else {
+        session_start();
         $_SESSION['errors'] = $errors;
-        redirect('/forms/register.php');
+        session_write_close();
     }
 }
 
@@ -81,10 +82,36 @@ function loginUser($email, $password)
     //create a connection variable using the db function in config.php
     $conn = db();
 
-    echo "<h1 style='color: red'> LOG ME IN (IMPLEMENT ME) </h1>";
+    // echo "<h1 style='color: red'> LOG ME IN (IMPLEMENT ME) </h1>";
     //open connection to the database and check if username exist in the database
-    //if it does, check if the password is the same with what is given
-    //if true then set user session for the user and redirect to the dasbboard
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($conn, $email);
+
+    $query = "SELECT * FROM students WHERE email =? LIMIT 1";
+    $stm = $conn->prepare($query);
+    $stm->bind_param('s', $email);
+    $stm->execute();
+    // $stm->bind_result($email);
+
+    $result = mysqli_stmt_get_result($stm);
+
+    if ($row = mysqli_fetch_assoc($result)) {
+        //if it does, check if the password is the same with what is given
+        $dbHashedPassword = $row['password'];
+        $checkPassword = password_verify($password, $dbHashedPassword);
+        if (!$checkPassword) {
+            redirect("forms/login.html?error=wrongcredentials");
+        } else {
+            //if true then set user session for the user and redirect to the dasbboard
+            session_start();
+            $_SESSION['username'] = $row['full_names'];
+            session_write_close();
+            redirect('dashboard.php');
+        }
+    } else {
+        echo 'Record does not exist in our database.';
+    }
+
 }
 
 
@@ -92,9 +119,33 @@ function resetPassword($email, $password)
 {
     //create a connection variable using the db function in config.php
     $conn = db();
-    echo "<h1 style='color: red'>RESET YOUR PASSWORD (IMPLEMENT ME)</h1>";
+    // echo "<h1 style='color: red'>RESET YOUR PASSWORD (IMPLEMENT ME)</h1>";
     //open connection to the database and check if username exist in the database
-    //if it does, replace the password with $password given
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($conn, $email);
+
+    $query = "SELECT * FROM students WHERE email =? LIMIT 1";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('s', $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($result)) {
+        //if it does, replace the password with $password given
+        $id = $row['id'];
+        $newPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $query = "UPDATE students SET password =? WHERE id =?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('ss', $newPassword, $id);
+        if (mysqli_stmt_execute($stmt)) {
+            echo "Password has been successfully changed.";
+        } else {
+            echo "Something went wrong";
+        }
+    } else {
+        echo "This email does not exist in our records.";
+    }
 }
 
 function getusers()
@@ -113,16 +164,14 @@ function getusers()
             //show data
             echo "<tr style='height: 30px'>" .
                 "<td style='width: 50px; background: blue'>" . $data['id'] . "</td>
-                <td style='width: 150px'>" . $data['full_names'] .
-                "</td> <td style='width: 150px'>" . $data['email'] .
-                "</td> <td style='width: 150px'>" . $data['gender'] .
-                "</td> <td style='width: 150px'>" . $data['country'] .
-                "</td>
+                   <td style='width: 150px'>" . $data['full_names'] . "</td> <td style='width: 150px'>" . $data['email'] .
+                "</td> <td style='width: 150px'>" . $data['gender'] . "</td> 
+                    <td style='width: 150px'>" . $data['country'] . "</td>
                 <form action='action.php' method='post'>
-                <input type='hidden' name='id'" .
-                "value=" . $data['id'] . ">" .
+                    <input type='hidden' name='id' value=" . $data['id'] . ">" .
                 "<td style='width: 150px'> <button type='submit', name='delete'> DELETE </button>" .
-                "</tr>";
+            "</tr>
+                </form>";
         }
         echo "</table></table></center></body></html>";
     }
@@ -133,5 +182,27 @@ function getusers()
 function deleteaccount($id)
 {
     $conn = db();
+
     //delete user with the given id from the database
+    $query = "DELETE FROM students WHERE id =? LIMIT 1";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('s', $id);
+    if (mysqli_stmt_execute($stmt)) {
+        echo "
+        <style>
+            .btn{
+                border: 1px solid #ccc;
+                background-color: #fcfc;
+                color:#333;
+                padding: 10px;
+                text-decoration: none;
+                cursor: pointer;
+            }
+        </style>
+        <p> Record deleted successfully.</p><br>
+        <a href='/zuri/week4_and_5/userAuthMySQL/dashboard.php' class='btn btn-primary'>Back</a>
+        ";
+    } else {
+        echo "Something went wrong!";
+    }
 }
